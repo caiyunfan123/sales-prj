d40a551825754a74b11d82c7bdf59c07a5aaa1ea
cyf123 <516691435@qq.com>
2019-08-02 14:49:37 +0800

    更新 购物篮模块-月组合top10

--- src/main/sql/03-dm/月销售组合top10.sql
+++ src/main/sql/03-dm/月销售组合top10.sql
@@ -1,11 +1,14 @@
 create table if not exists month_sales_combine_top10(
+c_year int,
+c_month int,
 product_sk1 int,
 product_sk2 int,
 total int
 )
 
 insert into month_sales_combine_top10
-select t1.product_sk,t2.product_sk,count(1) from dim_date t1
+select t1.year,t1.month,
+t1.product_sk,t2.product_sk,count(1) from dim_date t1
 join com_sales_order t2
 on t1.date_sk=t2.date_sk and t1.year=year(${input_year}) and t1.month = month(${input_month})
 group by t1.product_sk,t2.product_sk
