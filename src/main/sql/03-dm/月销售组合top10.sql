2ff7b6554e773e45bda73302487a5bc3263ce07e
cyf123 <516691435@qq.com>
2019-08-03 17:04:09 +0800

    测试了购物篮组合分析模块-月组合分析 的代码 经过数据实测，输出结果正确。

--- src/main/sql/03-dm/月销售组合top10.sql
+++ src/main/sql/03-dm/月销售组合top10.sql
@@ -1,3 +1,5 @@
+--经过数据实测，成功。
+
 create table if not exists month_sales_combine_top10(
 c_year int,
 c_month int,
@@ -6,11 +8,11 @@ product_sk2 int,
 total int
 )
 
-insert into month_sales_combine_top10
+insert into sales_dm.month_sales_combine_top10
 select t1.year,t1.month,
-t1.product_sk,t2.product_sk,count(1) from dim_date t1
+t2.product_sk1,t2.product_sk2,count(1) total from dim_date t1
 join com_sales_order t2
-on t1.date_sk=t2.date_sk and t1.year=year(${input_year}) and t1.month = month(${input_month})
-group by t1.product_sk,t2.product_sk
-order by count(1) desc
+on t1.date_sk=t2.date_sk and t1.year=${input_year} and t1.month = ${input_month}
+group by t1.year,t1.month,t2.product_sk1,t2.product_sk2
+order by total desc
 limit 10;
